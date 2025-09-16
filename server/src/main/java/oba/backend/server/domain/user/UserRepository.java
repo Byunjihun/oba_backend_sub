29885f2309c80f02141d69adfe0c1433a8fa1e53
Byunjihun <vkvlditm00@naver.com>
2025-09-16 17:05:19 +0900

    ON-57 UserRepository 생성 - JPA 기반 DB 접근

--- server/src/main/java/oba/backend/server/domain/user/UserRepository.java
+++ server/src/main/java/oba/backend/server/domain/user/UserRepository.java
@@ -1,4 +1,10 @@
 package oba.backend.server.domain.user;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByIdentifier(String identifier);
 }
+
