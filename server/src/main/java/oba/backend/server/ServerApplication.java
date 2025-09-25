4ca10217653bee65fd5c0107d702cbca39a9ae92
Byunjihun <vkvlditm00@naver.com>
2025-09-25 09:49:15 +0900

    ON-58 서버 실행 성공 log 추가

--- server/src/main/java/oba/backend/server/ServerApplication.java
+++ server/src/main/java/oba/backend/server/ServerApplication.java
@@ -1,13 +1,16 @@
 package oba.backend.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServerApplication {
+    private static final Logger log = LoggerFactory.getLogger(ServerApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
-        System.out.println(1);
+        log.info("ServerApplication started successfully!");
     }
 }
