30118a4c1f0cfce5838a5b1214f08b8d8b8d2636
jihun <vkvlditm00@naver.com>
2025-10-15 09:51:06 +0900

    ON-60 AiResponseDto 추가 - GPT 응답 데이터 구조 정의

--- server/src/main/java/oba/backend/server/dto/AiResponseDto.java
+++ server/src/main/java/oba/backend/server/dto/AiResponseDto.java
@@ -1,4 +1,12 @@
 package oba.backend.server.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class AiResponseDto {
+    private String result; // GPT 응답 텍스트 전체
 }
